dde632a0a19ef4062d1826d725f1243c81260b93
raiane-honorato <raianehonorato16@gmail.com>
2022-05-08 15:33:27 -0300

    feat(server): add port variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,6 +11,6 @@ app.use(routes);
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 
-app.listen(3333, () => {
-  console.log("http server running");
+app.listen(process.env.PORT || 3333, () => {
+  console.log("HTTP server running 🚀 ");
 });
